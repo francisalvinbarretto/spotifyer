e517cda2c5caba79629bdaf7458745cd880cf859
Francis Alvin Barretto <francis@Franciss-iMac.local>
2015-09-26 19:54:15 +0800

    removed log for app index

--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
 var sonata = require('./controllers/sonata');
 var player = require('./controllers/player');
 
-console.log('the player:', player);
 module.exports = {
 	controllers: {
 		sonata: sonata,
